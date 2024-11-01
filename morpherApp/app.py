f67e4e22576c15b77111903337375056089dd766
rslow <redmaxx4@gmail.com>
2024-11-01 22:10:17 +1000

    update for single server reverse-proxy

--- morpherApp/app.py
+++ morpherApp/app.py
@@ -1,6 +1,8 @@
+import os
+
 from fastapi import FastAPI
 
 from routes import morph_router
 
-app = FastAPI()
+app = FastAPI(root_path=os.getenv("MORPHER_ROOT_DIR"))
 app.include_router(morph_router)
